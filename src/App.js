f42b12ad68e19667e753997ab16516130bde1e54
zenott <mblevel@gmail.com>
2018-08-17 12:38:20 +0200

    Add buttons and add font awesome

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuoteLeft} from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faTumblr } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faQuoteLeft, faTwitter, faTumblr);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div id="quote-box">
-          <div id="text">Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</div>
+          <div id="text"><FontAwesomeIcon icon="quote-left" color="#00dbdb" size="lg" pull="left"/>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</div>
           <div id="author">Lorem ipsum</div>
           <div id="lower-div">
-            <button id="new-quote"></button>
-            <button id="tweet-quote"></button>
-            <button id="tumblr"></button>
+            <div id="social">
+              <button id="tweet-quote" className="button"><FontAwesomeIcon icon={faTwitter} size="lg" color="#ffffff"/></button>
+              <button id="tumblr" className="button"><FontAwesomeIcon icon={faTumblr} size="lg" color="#ffffff"/></button>
+            </div>
+            <button id="new-quote" className="button">New quote</button>
           </div>
         </div>
         <div id="sign">by zenott</div>
