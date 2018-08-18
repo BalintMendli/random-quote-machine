0ff60502f15446e9ab6352215841ab14c61d7fb6
zenott <mblevel@gmail.com>
2018-08-18 10:28:04 +0200

    Add animation

--- src/App.js
+++ src/App.js
@@ -13,29 +13,37 @@ class App extends Component {
     super(props);
     this.state={
       quote:'',
-      author:''
+      author:'',
+      fade:false
     };
     this.changeQuote=this.changeQuote.bind(this);
+    this.removeAnimClass=this.removeAnimClass.bind(this);
   }
   changeQuote(){
     let n=quotes.quotes.length;
     let qnum=Math.floor(n*Math.random());
     this.setState({      
       quote: quotes.quotes[qnum].quote,
-      author: quotes.quotes[qnum].author
+      author: quotes.quotes[qnum].author,
+      fade:true
     });
   }
   componentDidMount(){
     this.changeQuote();
+  }
+  removeAnimClass(){
+    this.setState({
+      fade:false
+    })
   }
   render() {
     return (
       <div className="App">
         <div id="quote-box">
           <div id="text-div">
-            <div id="text"><FontAwesomeIcon icon="quote-left" color="#00dbdb" size="sm" id="quote-icon"/> {this.state.quote}</div>
+            <div id="text" className={this.state.fade ? 'fade-in' : ''} onAnimationEnd={this.removeAnimClass}><FontAwesomeIcon icon="quote-left" color="#00dbdb" size="sm" id="quote-icon"/> {this.state.quote}</div>
           </div>
-          <div id="author">- {this.state.author}</div>
+          <div id="author" className={this.state.fade ? 'fade-in' : ''} >- {this.state.author}</div>
           <div id="lower-div">
             <div id="social">
               <button id="tweet-quote" className="button"><FontAwesomeIcon icon={faTwitter} size="lg" color="#ffffff"/></button>
