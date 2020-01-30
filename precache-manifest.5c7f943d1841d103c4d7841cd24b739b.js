fd75de333137fb77f153b8cb9f13dd9833b72b4a
zenott <mblevel@gmail.com>
2020-01-30 11:49:19 +0100

    Updates

---
+++ precache-manifest.5c7f943d1841d103c4d7841cd24b739b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96eff9df8f3b039086390d5cffe3702",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "fc4b0c4d692c9fe58ee3",
+    "url": "/random-quote-machine/static/css/main.2c821ec0.chunk.css"
+  },
+  {
+    "revision": "1a1f8570b6a672780e9b",
+    "url": "/random-quote-machine/static/js/2.416e0873.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/random-quote-machine/static/js/2.416e0873.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc4b0c4d692c9fe58ee3",
+    "url": "/random-quote-machine/static/js/main.e3ae83f2.chunk.js"
+  },
+  {
+    "revision": "afc9b93c50eb3e83906a",
+    "url": "/random-quote-machine/static/js/runtime-main.daced29d.js"
+  }
+]);
